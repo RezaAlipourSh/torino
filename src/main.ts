052d0df83a6f66f7bdbf394903b837cef9a3b840
rza <rezaa.ras1997@gmail.com>
2025-01-07 14:18:03 +0330

    feat: add validation pipe for request validation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./module/app/app.module";
 import { SwaggerConfig } from "./config/swagger.config";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    })
+  );
   SwaggerConfig(app);
   const { PORT, DB_HOST } = process.env;
   await app.listen(PORT, () => {
