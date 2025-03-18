b4e498fe9c2ae18f9ac9d1bb06543eeedae61d1b
rza <rezaa.ras1997@gmail.com>
2025-03-19 00:31:31 +0330

    refactor: remove unused code & dto

--- src/module/user/dto/update-user.dto.ts
+++ src/module/user/dto/update-user.dto.ts
@@ -1,5 +1,5 @@
 import { PartialType } from "@nestjs/mapped-types";
-import { AddUserBankAccountDto, CreateUserDto } from "./create-user.dto";
+import { CreateUserDto } from "./create-user.dto";
 import { ApiPropertyOptional } from "@nestjs/swagger";
 import {
   IsNumberString,
